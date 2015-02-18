203784fc598cef241f78e5f599855a72bb213089
Andrew VanVlack <A.VanVlack@Gmail.com>
2015-02-18 02:06:20 -0800

    Added Readme

--- twitter.js
+++ twitter.js
@@ -1,14 +1,19 @@
 var Twitter = require('twitter');
+var Key = require('./Key.js');
 
 var twit = new Twitter({
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: ''
+	consumer_key: Key.c_key,
+	consumer_secret: Key.c_secret,
+	access_token_key: Key.a_key,
+	access_token_secret: Key.a_secret
 })
 
-twit.post('statuses/update', {status: 'At work on my first fullstack project. A twitter dashboard for chromecast.'},  function(error, tweet, response){
-  if(error) throw error;
-  console.log(tweet);  // Tweet body. 
-  console.log(response);  // Raw response object. 
+twit.stream('statuses/filter', {track: 'apple'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+ 
+  stream.on('error', function(error) {
+    throw error;
+  });
 });
